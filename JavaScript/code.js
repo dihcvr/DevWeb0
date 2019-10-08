59d3ac7efea2cdc4c1cf701f5ef10d8e90970771
Client24@EST.LAN <Client24@EST.LAN>
2019-10-08 16:50:32 +0100

    tp2

---
+++ JavaScript/code.js
@@ -0,0 +1,37 @@
+/*
+let rep=prompt("une note : ");
+
+if(rep<10) console.log("redouble");
+else if (rep<12) console.log("passable");
+else if (rep<14) console.log("Assez bien");
+else if (rep<16) console.log("Bien");
+else if (rep<=20) console.log("Tres Bien");
+else console.log('note invalide');
+
+*/
+//Calculator
+/*
+let n1=Number.parseInt(prompt("first number "));
+let n2=Number.parseInt(prompt("second number "));
+let res;
+
+let oper=prompt("+ | / | * | - ");
+
+switch(oper){
+	case '+' : res=somme(n1,n2); break;
+	case '-' : res=n1 - n2; break;
+	case '*' : res=n1 * n2; break;
+	case '/' : res=n1 / n2; break;
+}
+console.log(res);
+
+function somme(a,b){
+	if (n1 && n2) {return a+b}
+		else return NAN;
+}
+*/
+
+function f=(a,b){
+	console.log(a+b);
+};
+f=(2,1);
